ceb547b9786db80ecfecfa0a96c3946f4d6672c8
Karina Zamudio <zamudiokazalo@gmail.com>
2024-07-08 00:32:53 -0600

    fixpanel operadores

--- views/paginas/operadoresadmin.php
+++ views/paginas/operadoresadmin.php
@@ -48,36 +48,4 @@
             <?php endforeach; ?>
         </tbody>
     </table>
-    <table class="propiedades1">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Imagen</th>
-                <th>Acciones</th>
-            </tr>
-        </thead>
-        <!-- Mostrar los resultados -->
-        <tbody>
-
-            <?php foreach ($operadores as $operador): ?>
-
-                <tr>
-                    <td> <?php echo $operador->id; ?> </td>
-                    <td> <?php echo $operador->nombre . " " .  $operador->apellido; ?> </td>
-                    <td> <img src="/imagenes/<?php echo $operador->imagen;?>" class="imagen-tabla"> </td>
-                    <td>
-                        <form method="POST" class="w-100" action="/nuestroEquipo/eliminar">
-                            <input type="hidden" name="id" value="<?php echo $operador->id; ?>">
-                            <input type="hidden" name="tipo" value="operador">
-                            <input type="submit" class="boton-rojo-block" value="Eliminar">
-                        </form>
-
-                        <a href="/nuestroEquipo/actualizar?id=<?php echo $operador->id; ?>"
-                        class="boton-rosa-block">Actualizar</a>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
 </main>

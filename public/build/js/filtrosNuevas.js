e5c825984753b22da08b00fec832b9f6e00d72d1
Karina Zamudio <zamudiokazalo@gmail.com>
2024-07-17 19:47:24 -0600

    try 2 Fix esconder boton

--- public/build/js/filtrosNuevas.js
+++ public/build/js/filtrosNuevas.js
@@ -1,4 +1,4 @@
-(function (){
+(function () {
     const orden = document.querySelector('#orden');
     const area = document.querySelector('#area');
     const maquina = document.querySelector('#maquina');
@@ -6,40 +6,40 @@
     const operador = document.querySelector('#operador');
     const contenedorForm = document.querySelector('#contenedorForm');
     const mobilemenu1 = document.querySelector('.mobile-menu1');
-    
+
     let ordenes = [];
 
     const mensajeNoResultados = document.createElement('div'); // Elemento para el mensaje de no resultados
     mensajeNoResultados.classList.add('alerta', 'error', 'oculto');
     mensajeNoResultados.textContent = 'No Hay Resultados, Intenta con otros términos de búsqueda';
     contenedorForm.insertBefore(mensajeNoResultados, contenedorForm.firstChild); // Insertar mensaje arriba del formulario
-    async function obtenerOrdenes(){
-        try{
+    async function obtenerOrdenes() {
+        try {
             const server = window.location.origin;
             const url = `${server}/api/ordenes`;
             const respuesta = await fetch(url);
             const datos = await respuesta.json();
-    
+
             ordenes = datos.vista_nuevas_ordenes;
             clientes = datos.vista_clientes;
             mostrar_botones = datos.mostrar_botones;
-            
+
             filtrar();
-        } catch (error){
+        } catch (error) {
             console.log(error);
 
         }
     }
-    
-    
+
+
     mobilemenu1.addEventListener('click', () => {
         contenedorForm.classList.toggle('oculto');
-    
+
     })
-    
-    
-    
-    
+
+
+
+
     const datosBusqueda = {
         orden: '',
         area: '',
@@ -47,7 +47,7 @@
         cliente: '',
         operador: ''
     }
-    
+
     document.addEventListener('DOMContentLoaded', function () {
         // filtros();
         $resultado = document.querySelector('tbody');
@@ -55,53 +55,53 @@
         obtenerOrdenes();
 
     });
-    
-    
+
+
     orden.addEventListener('input', e => {
         datosBusqueda.orden = e.target.value;
-    
-        filtrar(); 
+
+        filtrar();
     });
-    
+
     area.addEventListener('change', e => {
         datosBusqueda.area = e.target.value;
-    
+
         filtrar();
         // datosBusqueda.area = e.target.value;
     });
-    
+
     maquina.addEventListener('change', e => {
         datosBusqueda.maquina = e.target.value;
         filtrar();
         // datosBusqueda.area = e.target.value;
     });
-    
+
     cliente.addEventListener('change', e => {
         datosBusqueda.cliente = e.target.value;
         filtrar();
-    
+
         // datosBusqueda.area = e.target.value;
     });
     operador.addEventListener('change', e => {
         datosBusqueda.operador = e.target.value;
         filtrar();
-    
+
         // datosBusqueda.area = e.target.value;
     });
 
     const resultado = document.querySelector('tbody'); // Definir resultado aquí
 
     function mostrarOrdenes(ordenes) {
-         limpiarHTML();
-         
-         if(ordenes.length > 0){
+        limpiarHTML();
+
+        if (ordenes.length > 0) {
             ordenes.forEach(orden => {
                 const { numero_orden, descripcion_orden, hora_orden, fecha_orden, prioridad_orden, nombre_area, nombre_maquina, referencia_cliente, nombre_cliente, nombre_operador, apellido_operador, nombre_usuario, apellido_usuario, id } = orden;
-                
+
                 const [year, month, day] = fecha_orden.split('-');
-                const dateTime = new Date(year, month-1,day);
+                const dateTime = new Date(year, month - 1, day);
                 const fechaFormateada = dateTime.toLocaleDateString('es-ES');
-        
+
                 const row = document.createElement('tr');
                 row.innerHTML = `
                     <td>${numero_orden}</td>
@@ -113,43 +113,42 @@
                     <td>${referencia_cliente} ${nombre_cliente}</td>
                     <td>${nombre_operador} ${apellido_operador}</td>
                     <td>${nombre_usuario} ${apellido_usuario}</td>
-                    ${mostrar_botones !== false ? `
-                    <td>
-                        <form method="POST" class="w-100" action="/busquedanuevas/eliminar">
-                            <input type="hidden" name="id" value="${id}">
-                            <input type="hidden" name="tipo" value="nuevas_ordenes">
-                            <input type="submit" class="boton-rojo-block-1" value="Eliminar">
-                        </form>
-                        <a href="/busquedanuevas/actualizar?id=${id}" class="boton-verde-block-1">Actualizar</a>
-                        <form method="POST" class="w-100" action="/busquedanuevas/siguiente_area">
-                            <input type="hidden" name="id" value="${id}">
-                            <input type="hidden" name="tipo" value="nuevas_ordenes">
-                            <input type="submit" class="boton-azul-block-1" value="Área >>">
-                        </form>
-                    </td>
-                    ` : ''}
+                    ${mostrar_botones !== true ? '' : ` <td>
+                    <form method="POST" class="w-100" action="/busquedanuevas/eliminar">
+                        <input type="hidden" name="id" value="${id}">
+                        <input type="hidden" name="tipo" value="nuevas_ordenes">
+                        <input type="submit" class="boton-rojo-block-1" value="Eliminar">
+                    </form>
+                    <a href="/busquedanuevas/actualizar?id=${id}" class="boton-verde-block-1">Actualizar</a>
+                    <form method="POST" class="w-100" action="/busquedanuevas/siguiente_area">
+                        <input type="hidden" name="id" value="${id}">
+                        <input type="hidden" name="tipo" value="nuevas_ordenes">
+                        <input type="submit" class="boton-azul-block-1" value="Área >>">
+                    </form>
+                </td>
+            `}
                 `;
                 resultado.appendChild(row);
             });
-         } else{
+        } else {
             noResultado();
-         }
+        }
 
     }
-    
+
     // Limpiar HTML
-    function limpiarHTML(){
-        while(resultado.firstChild){
+    function limpiarHTML() {
+        while (resultado.firstChild) {
             resultado.removeChild(resultado.firstChild);
         }
     }
-    
+
     // function llenarSelect() {
-    
+
     //     const max = new Date().getFullYear();
     //     const min = max - 10;
-    
-    
+
+
     //     for (let i = max; i > min; i--) {
     //         const opcion = document.createElement('option');
     //         opcion.value = i;
@@ -157,68 +156,68 @@
     //         fecha.appendChild(opcion); // Agrega las opciones de año al select
     //     }
     // }
-    
-    
-    function filtrar(){
-        const resultadoFiltrado = ordenes.filter( filtrarOrden ).filter( filtrarFecha ).filter( filtrarArea ).filter( filtrarMaquina ).filter( filtrarCliente ).filter( filtrarNombre ).filter( filtrarApellido);
-        if(resultadoFiltrado.length){
+
+
+    function filtrar() {
+        const resultadoFiltrado = ordenes.filter(filtrarOrden).filter(filtrarFecha).filter(filtrarArea).filter(filtrarMaquina).filter(filtrarCliente).filter(filtrarNombre).filter(filtrarApellido);
+        if (resultadoFiltrado.length) {
             ocultarMensajeNoResultados();
             mostrarOrdenes(resultadoFiltrado);
-        }else{
+        } else {
             mostrarMensajeNoResultados();
             noResultado();
         }
     }
 
-    function mostrarMensajeNoResultados(){
+    function mostrarMensajeNoResultados() {
         mensajeNoResultados.classList.remove('oculto');
     }
 
-    function ocultarMensajeNoResultados(){
+    function ocultarMensajeNoResultados() {
         mensajeNoResultados.classList.add('oculto');
     }
-    
-    function noResultado(){  
+
+    function noResultado() {
         limpiarHTML();
     }
-    
-    
-    function filtrarOrden(ordenes){
+
+
+    function filtrarOrden(ordenes) {
         const { orden } = datosBusqueda;
-        if(orden){
+        if (orden) {
             return ordenes.numero_orden.includes(orden);
         }
         return ordenes;
     }
 
-    function filtrarFecha(ordenes){
+    function filtrarFecha(ordenes) {
         const { fecha } = datosBusqueda;
-        if(fecha){
+        if (fecha) {
             return ordenes.fecha_orden === fecha;
         }
         return ordenes;
     }
-    
-    function filtrarArea(ordenes){
+
+    function filtrarArea(ordenes) {
         const { area } = datosBusqueda;
-        if(area){
+        if (area) {
             return ordenes.nombre_area === area;
         }
         return ordenes;
     }
-    
-    function filtrarMaquina(ordenes){
+
+    function filtrarMaquina(ordenes) {
         const { maquina } = datosBusqueda;
-        if(maquina){
+        if (maquina) {
             return ordenes.nombre_maquina === maquina;
         }
         return ordenes;
     }
 
 
-    function filtrarCliente(ordenes){
+    function filtrarCliente(ordenes) {
         const { cliente } = datosBusqueda;
-        if(cliente){
+        if (cliente) {
             const partes = cliente.split(" ");
             return ordenes.referencia_cliente === partes[0];
 
@@ -226,21 +225,21 @@
         }
         return ordenes;
     }
-//oeradoresssssss
-    function filtrarNombre(ordenes){
+    //oeradoresssssss
+    function filtrarNombre(ordenes) {
         const { operador } = datosBusqueda;
-        if(operador){
+        if (operador) {
             const partes = operador.split(" ");
             return ordenes.nombre_operador === partes[0];
 
-            
+
         }
         return ordenes;
     }
 
-    function filtrarApellido(ordenes){
+    function filtrarApellido(ordenes) {
         const { operador } = datosBusqueda;
-        if(operador){
+        if (operador) {
             const partes = operador.split(" ");
             return ordenes.apellido_operador === partes[1];
 
